ba564f53ec365eb4c0ae8360b179e9849c3425e3
bmehder <bmehder@gmail.com>
2022-11-14 15:28:06 +0100

    third

--- src/routes/api/+server.json.js
+++ src/routes/api/+server.json.js
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit'
 
 export async function GET() {
-  return new Response(JSON.stringify({ hello: 'world' }), { status: 200 })
+  return json({ hello: 'world' }, { status: 200 })
 }
